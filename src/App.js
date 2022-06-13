0e0ce6a88dc6d9d569f9f82f8e6040316070866d
Abdinasir Mohamed <abdinasir400@gmail.com>
2022-06-13 14:02:02 +0100

    set up pages and started planning

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+import Dashboard from './routes/Dashboard';
+import Login from './routes/Login';
+import Register from './routes/Register';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Madrasa app</h1>
-    </div>
+    <>
+    <Router>
+      <div className = 'container'>
+        <Routes>
+          <Route path = '/' element = {<Dashboard />} />
+          <Route path = '/login' element = {<Login />} />
+          <Route path = '/register' element = {<Register />} />
+
+        </Routes>
+
+      
+      </div>
+      </Router>
+    
+    
+
+    </>
   );
 }
 
---
+++ src/routes/Dashboard.jsx
@@ -0,0 +1,15 @@
+import {useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
+import {useSelector} from 'react-redux' 
+import React from 'react'
+
+
+function Dashboard() {
+    return <div>  Dashboard</div>
+
+}
+
+export default Dashboard
+
+
+
